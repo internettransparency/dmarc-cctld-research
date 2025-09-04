b7b87e3a8337f78edd7222b90fb7d4e792c29570
Gustavo Luvizotto Cesar <g.luvizottocesar@utwente.nl>
2025-09-04 11:01:49 +0200

    update namings and analysis

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+python -m venv venv
+
 mkdir -p neo4j/{conf,data,import,logs,plugins}
-wget -O neo4j/plugins https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/4.4.0.37/apoc-4.4.0.37-all.jar
